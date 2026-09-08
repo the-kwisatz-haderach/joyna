51d6b276a2267504565e119fa93d391b26c4d254
Gustaf Lundström <31816977+the-kwisatz-haderach@users.noreply.github.com>
2026-09-08 11:51:28 +0200

    feat: add cleanup policies for af (#118)
    * feat: add cleanup policies for af
    
    * fix: depbot auto merge
    
    * fix: cleanup policy syntax

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -1,7 +1,34 @@
 resource "google_artifact_registry_repository" "joyna" {
-  location      = var.region
-  repository_id = var.artifact_repo_name
-  format        = "DOCKER"
+  location               = var.region
+  repository_id          = var.artifact_repo_name
+  format                 = "DOCKER"
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
 
+    condition {
+      tag_state  = "UNTAGGED"
+      older_than = "259200s"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = ["frontend", "api", "migrate"]
+      keep_count            = 5
+    }
+  }
+  cleanup_policies {
+    id     = "delete-old-versions"
+    action = "DELETE"
+
+    condition {
+      tag_state             = "TAGGED"
+      older_than            = "432000s"
+      package_name_prefixes = ["frontend", "api", "migrate"]
+    }
+  }
   depends_on = [google_project_service.apis]
 }
